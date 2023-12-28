0736e236043c65ee2c03ce6c2a562eeb024f59fe
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2023-12-28 23:09:35 +0200

    Modify cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express()
 require('dotenv').config()
 
 
-app.use(cors({origin: [process.env.CORS]}))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
